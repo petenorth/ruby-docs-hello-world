2f78371fb4745aa54eca6bd4a507295a9fa33e1c
Peter Fry <3152937+petenorth@users.noreply.github.com>
2019-01-21 19:06:12 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception 
   def hello
-    render html: "Hello, world from Azure App Service on Linux CHANGED!"
+    render html: "Hello, world from Azure App Service on Linux CHANGED 2!"
   end
 end
